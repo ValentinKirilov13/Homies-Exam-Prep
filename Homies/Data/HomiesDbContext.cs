d6cb07faaf27f39b13ed0e427afd3959c6d227b6
Valentin Kirilov <valkilmaar@gmail.com>
2024-02-11 20:10:29 +0200

    Edit DbContext and appsettings.json file

--- Homies/Data/HomiesDbContext.cs
+++ Homies/Data/HomiesDbContext.cs
@@ -11,30 +11,38 @@ namespace Homies.Data
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            //modelBuilder
-            //    .Entity<Type>()
-            //    .HasData(new Type()
-            //    {
-            //        Id = 1,
-            //        Name = "Animals"
-            //    },
-            //    new Type()
-            //    {
-            //        Id = 2,
-            //        Name = "Fun"
-            //    },
-            //    new Type()
-            //    {
-            //        Id = 3,
-            //        Name = "Discussion"
-            //    },
-            //    new Type()
-            //    {
-            //        Id = 4,
-            //        Name = "Work"
-            //    });
+            modelBuilder.Entity<EventParticipant>()
+                .HasKey(pk => new { pk.HelperId, pk.EventId });
 
-            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<EventParticipant>()
+                .HasOne(e => e.Event)
+                .WithMany(e => e.EventsParticipants)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder
+                .Entity<Type>()
+                .HasData(new Type()
+                {
+                    Id = 1,
+                    Name = "Animals"
+                },
+                new Type()
+                {
+                    Id = 2,
+                    Name = "Fun"
+                },
+                new Type()
+                {
+                    Id = 3,
+                    Name = "Discussion"
+                },
+                new Type()
+                {
+                    Id = 4,
+                    Name = "Work"
+                });
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
