2cb54dd48a1bd772a80d665e26c9f005c2617b92
Valentin Kirilov <valkilmaar@gmail.com>
2024-02-11 20:56:10 +0200

    Add Event Controller and  Get method All

---
+++ Homies/Controllers/EventController.cs
@@ -0,0 +1,41 @@
+﻿using Homies.Data;
+using Homies.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using static Homies.Common.Constants.DataConstants;
+
+namespace Homies.Controllers
+{
+    [Authorize]
+    public class EventController : Controller
+    {
+        private readonly HomiesDbContext _context;
+
+        public EventController(HomiesDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]   
+        public async Task<IActionResult> All()
+        {
+            var events = await _context.Events
+                .AsNoTracking()
+                .Select(x => new EventInfoViewModel()
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Start = x.Start.ToString(DateFormat),
+                    Type = x.Type.Name,
+                    Organiser = x.Organiser.UserName
+                })
+                .ToListAsync();
+
+            return View(events);
+        }
+
+
+
+    }
+}
---
+++ Homies/Data/Migrations/20240211181336_AddEntities.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Homies.Data.Migrations
+{
+    public partial class AddEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Types",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false, comment: "Type Identifier")
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false, comment: "Type Name")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Types", x => x.Id);
+                },
+                comment: "Tables of Types");
+
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false, comment: "Event Identifier")
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false, comment: "Event Name"),
+                    Description = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false, comment: "Event Description"),
+                    OrganiserId = table.Column<string>(type: "nvarchar(450)", nullable: false, comment: "Event Organiser Identifier"),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false, comment: "Create Date of Event"),
+                    Start = table.Column<DateTime>(type: "datetime2", nullable: false, comment: "Start Date of Event"),
+                    End = table.Column<DateTime>(type: "datetime2", nullable: false, comment: "End Date of Event"),
+                    TypeId = table.Column<int>(type: "int", nullable: false, comment: "Event Type Identifier")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Events_AspNetUsers_OrganiserId",
+                        column: x => x.OrganiserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Events_Types_TypeId",
+                        column: x => x.TypeId,
+                        principalTable: "Types",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                },
+                comment: "Table of Events");
+
+            migrationBuilder.CreateTable(
+                name: "EventsParticipants",
+                columns: table => new
+                {
+                    HelperId = table.Column<string>(type: "nvarchar(450)", nullable: false, comment: "Participant Identifier"),
+                    EventId = table.Column<int>(type: "int", nullable: false, comment: "Event Identifier")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventsParticipants", x => new { x.HelperId, x.EventId });
+                    table.ForeignKey(
+                        name: "FK_EventsParticipants_AspNetUsers_HelperId",
+                        column: x => x.HelperId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EventsParticipants_Events_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Events",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                },
+                comment: "Table with Participant for every Event");
+
+            migrationBuilder.InsertData(
+                table: "Types",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Animals" },
+                    { 2, "Fun" },
+                    { 3, "Discussion" },
+                    { 4, "Work" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_OrganiserId",
+                table: "Events",
+                column: "OrganiserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_TypeId",
+                table: "Events",
+                column: "TypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventsParticipants_EventId",
+                table: "EventsParticipants",
+                column: "EventId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EventsParticipants");
+
+            migrationBuilder.DropTable(
+                name: "Events");
+
+            migrationBuilder.DropTable(
+                name: "Types");
+        }
+    }
+}
